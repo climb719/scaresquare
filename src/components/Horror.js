101ab9afd00859641a839ebd2fe3581bd104bf92
climb719 <mary.oldweiler@gmail.com>
2021-10-07 09:53:31 -0400

    start vote event

--- src/components/Horror.js
+++ src/components/Horror.js
@@ -1,18 +1,25 @@
 import { Component } from 'react'
 
 class Horror extends Component {
-   
 
     
     
+    
+    onClick = (e) => {
+        console.log(this)
+    }
+   
+
+    
     render() {
+        const { title, year, descriptor, votes } = this.props
     return (
         <div class="card" >
-            <h2 class="title">{props.title}</h2>
-            <h3> {props.year}</h3>
-            <p>{props.descriptor}</p>
-            <p>{props.votes} Votes</p>
-            <button class="vote-btn">Vote</button>
+            <h2>{title}</h2>
+            <h3> {year}</h3>
+            <p>{descriptor}</p>
+            <p>{votes} Votes</p>
+            <button onClick={this.onClick}>Vote</button>
         </div>
         )
     }
