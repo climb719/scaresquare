0e48f2933131811e54cfa769e6ad26d3761d24d2
climb719 <mary.oldweiler@gmail.com>
2021-10-07 11:43:23 -0400

    refactor to use fetch for horrors from api

--- src/containers/HorrorList.js
+++ src/containers/HorrorList.js
@@ -1,6 +1,6 @@
 import { Component } from 'react'
 import Horror from '../components/Horror'
-import horrorsObj from '../data'
+//import horrorsObj from '../data'
 
 class HorrorList extends Component {
 
@@ -8,12 +8,28 @@ class HorrorList extends Component {
         horrors: []
     }
 
+    makeHorrorCard() {
+        return this.state.horrors.map(horror => <Horror id={horror.id} title={horror.title} year={horror.year} descriptor={horror.descriptor} votes={horror.votes}/>)
+    }
+
+    componentDidMount(){
+        const url ="http://localhost:3000/horrors"
+        fetch(url)
+        .then(resp => resp.json())
+        .then(json => {
+            console.log(json)
+            this.setState({
+                horrors: json
+            })
+        })
+    }
+
 
     render() {
-        const horrors = horrorsObj.horrors.map(horror => <Horror title={horror.title} year={horror.year} descriptor={horror.descriptor} votes={horror.votes}/>)
+        //const horrors = horrorsObj.horrors.map(horror => <Horror title={horror.title} year={horror.year} descriptor={horror.descriptor} votes={horror.votes}/>)
         return (
         <div id="horror-list">
-            {horrors}
+            {this.makeHorrorCard()}
         </div>
         )
     }
